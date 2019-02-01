e89837fee73cbcf988c97ee0c0dd95d31bc3ae57
Emilysavannahjames <43076722+Emilysavannahjames@users.noreply.github.com>
2019-02-01 12:31:40 -0700

    added FileController for the CrimeData vector

---
+++ DataStructures/Controller/FileController.cpp
@@ -0,0 +1,47 @@
+//
+//  FileController.cpp
+//  DataStructures
+//
+//  Created by James, Emily on 2/1/19.
+//  Copyright © 2019 CTEC. All rights reserved.
+//
+
+#include "FileController.hpp"
+
+vector<CrimeData>FileController :: readCrimeDataToVector(string filename)
+{
+    std:: vector<CrimeData> crimeVector;
+    string currentCSVLine;
+    int rowCount = 0;
+    
+    ifstream dataFile(filename);
+    
+    //if the file exists in that path
+    if(dataFile.is_open())
+    {
+        //keep reading until you are at end of line
+        while (!dataFile.eof())
+        {
+            //grab each line from csv
+            getline(dataFile, currentCSVLine, '\r');
+            //exclude header row
+            if(rowCount != 0)
+            {
+                if(currentCSVLine.length() != 0)
+                {
+                    CrimeData row(currentCSVLine);
+                    crimeVector.push_back(row);
+                }
+            }
+            rowCount++;
+        }
+        dataFile.close();
+    }
+    else
+    {
+        cerr << "NO FILE" << endl;
+    }
+    
+    return crimeVector;
+    
+}
---
+++ DataStructures/Controller/FileController.hpp
@@ -0,0 +1,20 @@
+//
+//  FileController.hpp
+//  DataStructures
+//
+//  Created by James, Emily on 2/1/19.
+//  Copyright © 2019 CTEC. All rights reserved.
+//
+
+#ifndef FileController_hpp
+#define FileController_hpp
+
+#include <stdio.h>
+
+class FileController
+{
+public:
+    static vector<CrimeData> readCrimeDataToVector(string filename);
+};
+
+#endif /* FileController_hpp */
